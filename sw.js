2e5381a5705b68ee7130d2a76fcd84e607078f84
GhostOmega-ai <joe.barnard1991@hotmail.com>
2026-06-11 01:54:46 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open('ghost-v2').then(cache => cache.addAll([
-    './',
+  self.skipWaiting();
+  event.waitUntil(caches.open('ghost-v3').then(cache => cache.addAll([
     './index.html',
     './manifest.json',
     './icon-192.png',
@@ -8,6 +8,12 @@ self.addEventListener('install', event => {
   ])));
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(
+    keys.filter(key => key !== 'ghost-v3').map(key => caches.delete(key))
+  )));
+});
+
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
