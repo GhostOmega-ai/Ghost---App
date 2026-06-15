99f33bdb7ad1bb45e9228ebb208f5406547674d9
GhostOmega-ai <joe.barnard1991@hotmail.com>
2026-06-15 12:54:19 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,4 @@
-self.addEventListener('install', event => {
-  self.skipWaiting();
-  event.waitUntil(caches.open('ghost-v3').then(cache => cache.addAll([
-    './index.html',
-    './manifest.json',
-    './icon-192.png',
-    './icon-512.png'
-  ])));
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(
-    keys.filter(key => key !== 'ghost-v3').map(key => caches.delete(key))
-  )));
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
-});
+const CACHE_NAME='ghost-v03';
+self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'])))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))));self.clients.claim()});
+self.addEventListener('fetch',event=>{event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)))});
